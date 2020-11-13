7cdd698d885e1c42e5d7d4a00fe7999552d10dd1
Ethan Harris <ethan.s.harris@protonmail.com>
2020-11-13 18:41:53 -0500

    Added drawer and two ways to open it, menu button and swipe

--- only_trips_mobile_app/lib/home.dart
+++ only_trips_mobile_app/lib/home.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:onlytrips/menudrawer.dart';
 
 class HomePage extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Text('OnlyTrips'),
         leading: IconButton(
@@ -11,11 +14,10 @@ class HomePage extends StatelessWidget {
             Icons.menu,
             semanticLabel: 'menu',
           ),
-          onPressed: () {
-            // TODO: Make menu do stuff
-          },
+          onPressed: () => _scaffoldKey.currentState.openDrawer(),
         ),
       ),
+      drawer: new NavDrawer(),
       body: Center(
         // TODO: Make stuff work in the main page
         child: Text('Landing Page'),
