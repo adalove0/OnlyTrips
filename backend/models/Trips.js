d065998f96eab8bd3513c0b208689493f2dbb96c
Ignacio <dps.ignacio@gmail.com>
2020-11-13 14:52:02 -0700

    added backend

---
+++ backend/models/Trips.js
@@ -0,0 +1,28 @@
+/*const mongoose = require('mongoose');
+
+const TravelSchema = new.mongoose.Schema({
+    numPeople:{
+        type: Number,
+        required: true
+    },
+    startingLocation:{
+
+    },
+    startDate:{
+        type:Date,
+        required: true
+    },
+    endDate:{
+        type: Date,
+        required: true
+    },
+    travelMethod:{
+        type:String,
+        required: true
+    },
+    destination: [{
+        city: {type: String, required :true},
+        country: {type: String, required:true }
+    }]
+
+})*/
---
+++ backend/models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+//const Trips = require('Trips');
+    
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        minlength: 3,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 100,
+        min: 4
+    },
+    age: {
+        type: Number,
+        required: true,
+        default: -1
+    },
+    location: [{
+        city: {type:String, required: true}, 
+        state: {type:String, default: ' '},
+        country: {type: String, required: true},
+        zip: {type: Number, min:00001, max:99999, required: true}
+    }],
+    confirmed:{
+        type: Boolean,
+        default: false
+    },
+    token: {
+        type: String
+    }
+
+})
+
+UserSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+UserSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
