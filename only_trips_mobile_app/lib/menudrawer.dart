bd3b1086676c1dd6da4776d91dd5136eb4b528c0
Ethan Harris <ethan.s.harris@protonmail.com>
2020-11-13 19:04:00 -0500

    Initial adding drawer

---
+++ only_trips_mobile_app/lib/menudrawer.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Side menu',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.green,
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage('assets/images/cover.jpg'))),
+          ),
+          ListTile(
+            leading: Icon(Icons.input),
+            title: Text('Home'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.verified_user),
+            title: Text('Profile'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () => {
+              // TODO - Create logout function/delete session
+              (_) => Navigator.of(context).pushReplacementNamed('/start')
+
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
