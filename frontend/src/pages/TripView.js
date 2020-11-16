77ffdc0561e86faa57e4f7c9827a74de22ee9b0e
Class1ck <justtincortes@knights.ucf.edu>
2020-11-16 05:33:39 -0500

    Added Logo, and log out button

--- frontend/src/pages/TripView.js
+++ frontend/src/pages/TripView.js
@@ -1,10 +1,31 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import App from "../App";
+import logo from "../images/OnlyTrips.svg";
 import LoggedInName from "../components/LoggedInName";
 import NavBar from "../components/NavBar";
 import UserProfile from "../components/UserProfile";
-const TripView = () => {
-  return <div>                    </div>;
-};
+import Alert from "react-bootstrap/Alert";
+
+function TripView(){
+  var something;
+
+  
+  /*const tView = () => 
+  {
+    return <div class>                    </div>;
+  };*/
+
+  const LogOut= async (event) => {
+    event.preventDefault();
+    window.location.href = "/";
+  };
+
+  return (
+    <div id="TopPart"> 
+      <img src={logo} alt="OnlyTrips Logo" id="TripViewLogo"></img>
+      <button id ="LogOutButton" onClick={LogOut}>Log Out</button>
+    </div>
+  );
+}
 
 export default TripView;
