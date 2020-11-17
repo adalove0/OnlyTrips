fb3c8e948e5c763e757be975fa5bf2cfe208d882
Ethan Harris <ethan.s.harris@protonmail.com>
2020-11-17 00:17:49 -0500

    I did things, maybe things made it work

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const path = require('path');
 
-app.set('port', (process.env.PORT || 5000));
 
 app.use(cors());
 app.use(bodyParser.json());
