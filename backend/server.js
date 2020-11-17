2134dc6f775d9799950dc21038cf21eb489c89a9
Ethan Harris <ethan.s.harris@protonmail.com>
2020-11-16 23:25:15 -0500

    const path = blahblahblah

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
-
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
