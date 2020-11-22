28d7aa98cb3ec450beb66ec9b53a6619db6cef93
esther-j98 <70731503+esther-j98@users.noreply.github.com>
2020-11-22 17:49:16 -0500

    Update AddTrip.js

--- frontend/src/components/AddTrip.js
+++ frontend/src/components/AddTrip.js
@@ -1,9 +1,9 @@
+  
 import React, {Component, useEffect, useState, useRef } from "react";
 import App from "../App";
 import logo from "../images/OnlyTrips.svg";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
-import DateCalender from "../components/DateCalender";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
@@ -23,15 +23,18 @@ class AddTrip extends Component {
       passengerNoun : "Traveler",
       startDate : null,
       endDate : null,
-      destination :["orlando", "florida"]
+      city: "",
+      State: "",
+      error: ""
     };
     
     this.handleChange = this.handleChange.bind(this);
     this.doAddTrip = this.doAddTrip.bind(this);
   }
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleChange=(e)=>{
+    this.setState ({ [e.target.name]: e.target.value });
   }
+
   increment = () => {
     this.setState({ count: this.state.count + 1});
     if(this.state.count > 1)
@@ -43,24 +46,54 @@ class AddTrip extends Component {
     if(this.state.count == 1)
       this.setState({ passengerNoun : "Traveler"});
   }
+  getMonth = (c) => {
+    if(c == "Jan")
+      return 1;
+    else if( c == "Feb")
+      return 2; 
+    else if( c == "Mar")
+      return 3;
+    else if(c == "Apr")
+      return 4;
+    
+  }
 
   async doAddTrip(e){
+    //this.state.error = "";
     const userObj = localStorage.getItem("user_data");
     const localUser = JSON.parse(userObj);  
+    /*const localUser =  {
+      email: "jamila@email.com",
+      id: "56789"
+    }*/
     const destinationLoc = ["orlando","florida"];   //Test destination: needs to be fixed
-    var obj = '{"creator" :"'+localUser+'", "numPeople" :"' +this.state.count+'", "startDate" :"' +this.state.startDate + '", "endDate" :"' +this.state.endDate+ '", "destination" : "' + destinationLoc + '"}';
-    /*var obj = {
-      "creator": localUser,
-      "numPeople": this.state.count,
-      "startDate": this.state.startDate,
-      "endDate": this.state.endDate,
-      "destination": ["orlando","florida"]
-      };*/
-      alert(obj);
+    var obj = {
+      email: localUser.email,
+      trip : {
+      creator: localUser.id,
+      numPeople: this.state.count,
+      startDate : this.state.startDate,
+      endDate : this.state.endDate,
+       destination: {
+         city :this.state.city, 
+         state:this.state.State
+        }
+      }
+    };
+      var js = JSON.stringify(obj);
+      localStorage.setItem("trip_data", JSON.stringify(js));
+      alert(js);
+      console.log(js);
+      /*if(this.state.startDate == null || this.state.endDate == null ||this.state.city == "" || this.state.State == "")
+      {
+        this.setState({error : "NO"});
+        alert(this.state.error);
+        return;
+      }*/
       try {
         const response = await fetch("http://localhost:3000/addtrip", {
           method: "POST",
-          body: JSON.stringify(obj),
+          body: js,
           headers: { "Content-Type": "application/json" },
         });
   
@@ -72,46 +105,54 @@ class AddTrip extends Component {
   }
 };
   render(){
-  return (
-    <div class = "addTrip">
-    <div id = "addTrip1" className = "row">
-      <div className = "column1">
-        <img src={logo} alt="OnlyTrips Logo" id="logo1"></img>
-      </div>
-      <div  class = "column2">
-        <h1 id= "add-title">ADD TRIP</h1>
-        <form id="add-trip-form">
-              {" "}
-          <input
-            type="text" 
-            id ="dest-input"
-            placeholder="Destination"
-            onChange={this.handleChange}
-					  //value={this.state.destination}
-            />
-          <br />       {" "}
-          <DateRangePicker cssClass="e-custom-style"
+    return (
+      <div class = "addTrip">
+        <div id = "circle">
+          <a href="#" class="close"/>
+          <img src={logo} alt="OnlyTrips Logo" id="logo"></img>
+        </div>
+          <h1 id= "add-title">ADD TRIP</h1>
+          <form id="add-trip-form">
+                {" "}
+            <input
+              type="text" 
+              className = "inputMaterial"
+              onChange = {e => this.setState({ city: e.target.value })}
+              placeholder = "City"
+              required="required"
+              />
+              <br/><br/>
+            <input
+              type="text" 
+              className = "inputMaterial"
+              onChange = {e => this.setState({ State: e.target.value })}
+              placeholder = "State"
+              />
+            <div className = "calendar">
+            <DateRangePicker cssClass="e-custom-style"
             startDate = {this.state.startDate}
             startDateId = "your_unique_start_date_id"
             endDate = {this.state.endDate}
             endDateId = "your_unique_end_date_id"
             onDatesChange = {({ startDate, endDate }) => this.setState({ startDate, endDate})}
             focusedInput = {this.state.focusedInput}
+            required="required"
             onFocusChange = {focusedInput => this.setState({ focusedInput})}
           />
+         </div>
           <br/>
           <div className="number">
 	          <div className="minus"  onClick={this.decrement}>-</div>
-	          <div className ="input-text-counter"><input type="text" value={`${this.state.count} ${this.state.passengerNoun}`} onChange={this.handleChange} min={0}/></div>
+	          <div className ="input-text-counter"><input id = "numPeople" type="text" value={`${this.state.count} ${this.state.passengerNoun}`} onChange={this.handleChange} min={0} required="required"/></div>
 	          <div className="plus"  onClick={this.increment}>+</div>
           </div>
         </form>
         <button className ="submit-btn" onClick = {this.doAddTrip}>Submit</button>
+        <p value = {this.state.error}></p>
       </div>
-    </div>
-    </div>
+
     );
   }
 }
-
+  
 export default AddTrip;
