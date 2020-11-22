7c79c915ca9535e8b3b943894b368a2eeffe32e7
Ethan Harris <ethan.s.harris@protonmail.com>
2020-11-22 02:35:14 -0500

    Changed from arrow notation to add init for shared prefs and to add a notifier for theme data.

--- only_trips_mobile_app/lib/main.dart
+++ only_trips_mobile_app/lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:onlytrips/app.dart';
+import 'package:onlytrips/shared_prefs.dart';
+import 'package:onlytrips/themes.dart';
+import 'package:provider/provider.dart';
 
 // Starts app located in lib/app.dart
-void main() => runApp(OnlyTripsApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await sharedPrefs.init();
+  runApp(ChangeNotifierProvider<ThemeNotifier>(
+    create: (_) =>
+        ThemeNotifier(sharedPrefs.isDarkMode ? darkTheme : lightTheme),
+    child: OnlyTripsApp(),
+  ));
+}
