bdfdff4653c30207ed36e4c7d96910937f71c487
esther-j98 <70731503+esther-j98@users.noreply.github.com>
2020-11-22 17:51:53 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,6 +24,9 @@ function App() {
         <Route path="/SignUpPage" exact>
           <SignUpPage />
         </Route>
+         <Route path="/AddTripPage" exact>
+          <AddTripPage />
+         </Route>
         <Route path="/TripView" exact>
           <TripView />
         </Route>
