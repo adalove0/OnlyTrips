ff4f0cda304ebb3107f9b8825be351b990bd6df8
Ignacio <dps.ignacio@gmail.com>
2020-11-23 16:11:12 -0700

    added retval

--- backend/routes/verifyEmail.js
+++ backend/routes/verifyEmail.js
@@ -7,7 +7,20 @@ router.post('/', (req, res, next)=>{
     User.findOne({token: req.body.token}, function(error,obj){
         User.token = null;
         User.confirmed = true;
-        User.save();
+        User.save((err, user) => {
+          if (err) {
+            console.log(err);
+            return res.send({
+              success: false,
+              message: "Error: Server Error",
+            });
+          } else {
+            return res.status(200).json({
+              success: true,
+              message: "Verification successful",
+            });
+          }
+        });
     })
 
 });
