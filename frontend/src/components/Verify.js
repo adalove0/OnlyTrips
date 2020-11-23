55329a9041c1b54c441ab83b3d2659e47fd11145
Ahmed Al Busaidi <36058005+TheOGRafiki@users.noreply.github.com>
2020-11-23 18:21:57 -0500

    Update Verify.js

--- frontend/src/components/Verify.js
+++ frontend/src/components/Verify.js
@@ -7,11 +7,8 @@ import Modal from "react-bootstrap/esm/Modal";
 import logo from "../images/updated-logo.PNG";
 
 function Verify() {
-  const userObj = localStorage.getItem("user_data");
-  const localUser = JSON.parse(userObj);
-  const name = localUser.fullName;
+
   const doVerify = async (event) => {
-    var data = { __id: localUser.id };
     var js = new URLSearchParams(window.location.hash).get('token');
     alert(js);
 
@@ -44,7 +41,7 @@ function Verify() {
     <div className ="header"><h1>WELCOME TO ONLY TRIPS</h1></div><div className = "text-info"><p id = "congrats-text">Congratulations, {name}</p>
           <p id = "confirm-text"> You're one step away from joining the onlyTrips family, activate your account by clicking on the button below:</p>
           <div>
-            <button id = "activate-btn">ACTIVATE ACCOUNT</button>
+            <button id = "activate-btn" onClick={doVerify}>ACTIVATE ACCOUNT</button>
         </div>
       </div>
       </div>
