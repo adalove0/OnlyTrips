535952deb1c8a75f6459fdb970967617a35a0b8f
Ahmed Al Busaidi <36058005+TheOGRafiki@users.noreply.github.com>
2020-11-23 18:42:15 -0500

    Add files via upload

---
+++ frontend/src/pages/UpdateTripPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Updaterip from '../components/UpdateTrip';
+
+const UpdateTripPage = () =>
+{
+    return(
+      <div>
+        <UpdateTrip />
+      </div>
+    );
+};
+
+export default UpdateTripPage;
