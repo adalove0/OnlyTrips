910da493225dc47c7ccdb8d040c309fd8f7dddaf
esther-j98 <70731503+esther-j98@users.noreply.github.com>
2020-11-23 11:14:03 -0500

    Update TripTable.js

--- frontend/src/components/TripTable.js
+++ frontend/src/components/TripTable.js
@@ -144,7 +144,7 @@ function TripView() {
             <div className = "TripTable">
               <div className = "buttons">
                 <div className="right-icons">
-                  <div className = "edit-icon"><div className = "edit-icon">
+                 <div className = "edit-icon">
                     <i className="fa">&#xf044;</i>
                   </div>
                   <div className = "delete-icon">
